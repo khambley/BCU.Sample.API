5927314e8e7abdd7abf2e4955cd43c0d9fe01cd2
Katherine <superdesigngirl@mac.com>
2019-12-04 13:32:45 -0600

    added Components class with decoupled implementation

---
+++ BCU.Sample.API/Components.cs
@@ -0,0 +1,39 @@
+﻿
+namespace BCU.Sample.API
+{
+    public interface IComponent
+    {
+        string Name { get; }
+    }
+
+    public class ComponentA
+    {
+        private readonly IComponent _componentB;
+
+        public ComponentA(IComponent componentB)
+        {
+            this._componentB = componentB;
+        }
+    }
+
+    public class ComponentB : IComponent
+    {
+        public string Name { get; set; } = nameof(ComponentB);
+    }
+    // this is the tightly-coupled version
+    //public class ComponentA
+    //{
+    //    private readonly ComponentB _componentB;
+
+    //    public ComponentA()
+    //    {
+    //        this._componentB = new ComponentB();
+    //    }
+    //}
+
+    //public class ComponentB
+    //{
+    //    public string Name { get; set; }
+
+    //}
+}
